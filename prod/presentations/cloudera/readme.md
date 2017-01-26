0d41650a3a61a745b6d7d64fb6be1520033118fc
nathan <nathan@rstudio.com>
2017-01-26 17:53:51 +0000

    reference for audience

---
+++ prod/presentations/cloudera/readme.md
@@ -0,0 +1,15 @@
+# Demo using CDH 5.9
+
+This repos contains files for demonstrating Spark and R on Cloudera using spakrlyr.
+
+### Scripts
+
+* Taxi Demo
+* Livy Connection
+* Histogram wrappers
+* Raster wrappers
+
+### Reports
+
+* [Data Science Toolchain with Spark and R](http://colorado.rstudio.com:3939/content/262/taxiDemoCloudera3.nb.html)
+* [Connecting to Spark through Livy](http://colorado.rstudio.com:3939/content/259/livy.nb.html)
---
+++ prod/presentations/cloudera/sqlvis_raster.R
@@ -0,0 +1,106 @@
+### Big data tile plot
+
+# data <- tbl(sc, "trips_model_data")
+# x_field <- "pickup_longitude"
+# y_field <- "pickup_latitude"
+# resolution <- 50
+
+sqlvis_compute_raster <- function(data, x_field, y_field, resolution = 300){
+  
+  data_prep <- data %>%
+    select_(x = x_field, y = y_field) %>%
+    filter(!is.na(x), !is.na(y))
+  
+  s <- data_prep %>%
+    summarise(max_x = max(x),
+              max_y = max(y), 
+              min_x = min(x),
+              min_y = min(y)) %>%
+    mutate(rng_x = max_x - min_x,
+           rng_y = max_y - min_y,
+           resolution = resolution) %>%
+    collect()
+  
+  counts <- data_prep %>% 
+    mutate(res_x = round((x - s$min_x) / s$rng_x * resolution, 0),
+           res_y = round((y - s$min_y) / s$rng_y * resolution, 0)) %>%
+    count(res_x, res_y) %>%
+    collect
+
+  list(counts = counts,
+       limits = s,
+       vnames = c(x_field, y_field)
+       )  
+
+}
+
+sqlvis_ggplot_raster <- function(data, ...) {
+
+  d <- data$counts
+  s <- data$limits
+  v <- data$vnames
+
+  xx <- setNames(seq(1, s$resolution, len = 6), round(seq(s$min_x, s$max_x, len = 6),2))
+  yy <- setNames(seq(1, s$resolution, len = 6), round(seq(s$min_y, s$max_y, len = 6),2))
+
+  ggplot(d, aes(res_x, res_y)) + 
+    geom_raster(aes(fill = n)) +
+    coord_fixed() +
+    scale_fill_distiller(palette = "Spectral", trans = "log", name = "Frequency") +
+    scale_x_continuous(breaks = xx, labels = names(xx)) +
+    scale_y_continuous(breaks = yy, labels = names(yy)) +
+    labs(x = v[1], y = v[2], ...)
+    
+}    
+
+### Facets
+
+sqlvis_compute_raster_g <- function(data, x_field, y_field, g_field, resolution = 300){
+  
+  data_prep <- data %>%
+    mutate_(group = g_field) %>%
+    select_(g = "group", x = x_field, y = y_field) %>%
+    filter(!is.na(x), !is.na(y))
+  
+  s <- data_prep %>%
+    summarise(max_x = max(x),
+              max_y = max(y), 
+              min_x = min(x),
+              min_y = min(y)) %>%
+    mutate(rng_x = max_x - min_x,
+           rng_y = max_y - min_y,
+           resolution = resolution) %>%
+    collect()
+  
+  counts <- data_prep %>% 
+    mutate(res_x = round((x-s$min_x)/s$rng_x*resolution, 0),
+           res_y = round((y-s$min_y)/s$rng_y*resolution, 0)) %>%
+    count(g, res_x, res_y) %>%
+    collect
+  
+  list(counts = counts,
+       limits = s,
+       vnames = c(x_field, y_field)
+  )  
+  
+}
+
+sqlvis_ggplot_raster_g <- function(data, ncol = 4, ...) {
+  
+  s <- data$limits
+  d <- data$counts
+  v <- data$vnames
+  
+  xx <- setNames(seq(1, s$resolution, len = 3), round(seq(s$min_x, s$max_x, len = 3), 1))
+  yy <- setNames(seq(1, s$resolution, len = 3), round(seq(s$min_y, s$max_y, len = 3), 1))
+  
+  ggplot(d, aes(res_x, res_y)) + 
+    geom_raster(aes(fill = n)) +
+    coord_fixed() +
+    facet_wrap(~ g, ncol = ncol) +
+    scale_fill_distiller(palette = "Spectral", trans = "log", name = "Frequency") +
+    scale_x_continuous(breaks = xx, labels = names(xx)) +
+    scale_y_continuous(breaks = yy, labels = names(yy)) +
+    labs(x = v[1], y = v[2], ...)
+  
+}    

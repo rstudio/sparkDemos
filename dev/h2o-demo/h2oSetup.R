7b2cd9ee492d68157376d243f036510033c773db
Your Name <you@example.com>
2017-02-01 16:04:03 +0000

    working with h2o versions and installs

--- dev/h2o-demo/h2oSetup.R
+++ dev/h2o-demo/h2oSetup.R
@@ -1,52 +1,26 @@
-# Remove previous versions of h2o R package
-if ("package:h2o" %in% search()) detach("package:h2o", unload=TRUE)
-if ("h2o" %in% rownames(installed.packages())) remove.packages("h2o")
+### rsparkling hello world
+### requires R packages: statmod, RCurl, and devtools
 
-# Next, we download R package dependencies
-pkgs <- c("methods","statmod","stats","graphics",
-          "RCurl","jsonlite","tools","utils")
-for (pkg in pkgs) {
-  if (!(pkg %in% rownames(installed.packages()))) install.packages(pkg)
-}
+### Install
 
-# Download h2o package version 3.10.0.6
+# install h2o version 3.10.0.6
 install.packages("h2o", type = "source", 
                  repos = "http://h2o-release.s3.amazonaws.com/h2o/rel-turing/6/R")
 
-library(devtools)
+# install rspakrling version 1.6.7
 devtools::install_github("h2oai/rsparkling", ref = "stable")
 
-### 
+# install spark version 1.6.0
+spark_install(version = "1.6.0") # for local
 
-library(sparklyr)
-spark_install(version = "1.6.2")
-options(rsparkling.sparklingwater.version = "1.6.2") # Use Sparkling Water 1.6.2 to match Spark version
+### Run
 
 library(sparklyr)
 library(rsparkling)
 library(dplyr)
 
 Sys.setenv(SPARK_HOME="/usr/lib/spark")
-config <- spark_config()
-config$spark.dynamicAllocation.enabled <- "false"
 options(rsparkling.sparklingwater.version = '1.6.7')
-sc <- spark_connect(master = "yarn-client", config = config, version = '1.6.2')
-airlines_tbl <- tbl(sc, "airlines")
-h2oframe <- as_h2o_frame(sc, airlines_tbl)
 
-
-mtcars_tbl <- copy_to(sc, mtcars, "mtcars", overwrite = TRUE)
-
-partitions <- mtcars_tbl %>%
-  filter(hp >= 100) %>%
-  mutate(cyl8 = cyl == 8) %>%
-  sdf_partition(training = 0.5, test = 0.5, seed = 1099)
-
-training <- as_h2o_frame(sc, partitions$training)
-test <- as_h2o_frame(sc, partitions$test)
-
-install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-turing/6/R")))
-library(h2o)
-localH2O = h2o.init(nthreads=-1)
-
+sc <- spark_connect(master = "yarn-client", config = conf, version = '1.6.0')
 
